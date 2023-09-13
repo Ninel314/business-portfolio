7d501c27808f33059fad23614a7cda596c987e60
Ninel314 <61876915+Ninel314@users.noreply.github.com>
2023-09-13 21:38:09 +0000

    Add persoal details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Ninel Pomazova",
+  title: ".NET Developer",
+  email: "ninel.p.o.k.j.1@gmail.com",
+  gitHub: "Ninel314",
+  linkedIn: "ninel-pomazova-7a7950171"
 };
 
 const primaryColor = "#4E567E";
