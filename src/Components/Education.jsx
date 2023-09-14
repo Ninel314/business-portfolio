3fb1511c858b50cac01d232fc2696fa5108473e1
Ninel314 <61876915+Ninel314@users.noreply.github.com>
2023-09-14 15:47:09 +0000

    Minor changes

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -4,31 +4,32 @@ import image from "../images/wunu.png";
 const imageAltText = "University image";
 
 const educationList = [
-    {
-        name: `West Ukrainian National University, Ternopil, Ukraine`,
-        faculty: `Faculty of Computer Information Technology`,
-        specialty: `Software Engineering`,
-        degree: `Master's degree`,
-    },
-    {
-        name: `IT company ELEKS`,
-        faculty: `Backend Development Training Courses with ASP.NET Core 3.1 MVC`,
-        degree: `Internship`,
-    },
-    {
-        name: `West Ukrainian National University, Ternopil, Ukraine`,
-        faculty: `Faculty of Computer Information Technology`,
-        specialty: `Software Engineering`,
-        degree: `Bachelor's degree`,
-    },  
+  {
+    name: `West Ukrainian National University, Ternopil, Ukraine`,
+    faculty: `Faculty of Computer Information Technology`,
+    specialty: `Software Engineering`,
+    degree: `Master's degree`,
+  },
+  {
+    name: `IT company ELEKS`,
+    faculty: `Backend Development Training Courses with ASP.NET Core 3.1 MVC`,
+    specialty: `Software Engineering`,
+    degree: `Internship`,
+  },
+  {
+    name: `West Ukrainian National University, Ternopil, Ukraine`,
+    faculty: `Faculty of Computer Information Technology`,
+    specialty: `Software Engineering`,
+    degree: `Bachelor's degree`,
+  }
 ];
 
 const Education = () => {
   return (
-      <section className="padding" id="education">
+    <section className="padding" id="education">
       <h2 style={{ textAlign: "center" }}>Education</h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "2rem" }}>
-      <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
             src={image}
             style={{ height: "90%", width: "100%", objectFit: "cover" }}
